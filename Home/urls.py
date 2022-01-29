761a13ef7aa13dbbec637330bebb982ece891d5a
TechnoStartOfficial <rambpandey238@gmail.com>
2022-01-29 11:29:02 +0530

    urlpattern function error

--- Home/urls.py
+++ Home/urls.py
@@ -4,4 +4,5 @@ from Home import views
 
 urlpatterns = [
     path("",views.index,name='home')
+    path("about",views.about,name='about')
 ]
--- Home/views.py
+++ Home/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("This Is Homepage")
+def about(request):
+    return HttpResponse("This Is About")
