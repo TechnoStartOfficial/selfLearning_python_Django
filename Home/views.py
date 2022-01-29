c714818e1a4cb1fe11048b7219e46077a61dc57d
TechnoStartOfficial <rambpandey238@gmail.com>
2022-01-29 19:43:40 +0530

    Django: Updated index with tempelate

--- Home/views.py
+++ Home/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("This Is Homepage")
+    # return HttpResponse("This Is Homepage") Now Linking With Template
+    return render(request,'index.html')
 def about(request):
     return HttpResponse("This Is About")
 def services(request):
