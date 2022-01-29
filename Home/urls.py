17f8fc3c955087e883b1dbcff7cc735733502a08
TechnoStartOfficial <rambpandey238@gmail.com>
2022-01-29 11:30:06 +0530

    Django: URL function Error fixed added ','

--- Home/urls.py
+++ Home/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from Home import views
 
 urlpatterns = [
-    path("",views.index,name='home')
-    path("about",views.about,name='about')
+    path("",views.index,name='home'),
+    path("about",views.about,name='about'),
 ]
