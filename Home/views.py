6591d191a3dbe99cb9db219199e49e38d812a236
TechnoStartOfficial <rambpandey238@gmail.com>
2022-01-30 11:07:15 +0530

    Django: Added more Variables

--- Home/views.py
+++ Home/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def index(request):
     context = {
-        'variable':'this is sent'
+        'variable1':'THIS IS SENT',
+        'variable2':'THIS IS SENT',
     }
     # return HttpResponse("This Is Homepage") Now Linking With Template
     return render(request,'index.htm', context)
